43453c4af9ad66ed333362badd8b1f5fcfcfd2f8
emilyliu75 <150020746+emilyliu75@users.noreply.github.com>
2024-06-07 17:04:06 +0100

    Update user.js

--- api/models/user.js
+++ api/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, trim: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, maxlength: 60},
   username: { type: String, required: true, unique: true, trim: true },
 });
 
